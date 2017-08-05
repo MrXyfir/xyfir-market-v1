face96539541b9028fd506c998cc59637b06fe56
MrXyfir <mr@xyfir.com>
2017-08-05 16:12:37 -0700

    Add commands/revise

---
+++ commands/revise.js
@@ -0,0 +1,29 @@
+const create = require('commands/create');
+const mysql = require('lib/mysql');
+
+/**
+ * Revise an edited unapproved sales thread and repost it if valid.
+ * @param {snoowrap} r
+ * @param {snoowrap.Comment} comment
+ */
+module.exports = async function(r, comment) {
+
+  const db = new mysql;
+
+  try {
+    // Load the thread that the comment resides in
+    const post = await r
+      .getSubmission(comment.context.split('/')[4])
+      .fetch();
+
+    // Only the creator can revise their own thread
+    if (comment.author.name != post.author.name) return;
+
+    create(r, post, true);
+  }
+  catch (err) {
+    db.release();
+    comment.reply(err);
+  }
+
+}
