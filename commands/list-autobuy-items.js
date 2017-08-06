ca594b201ee5582f99842561e5141cd6fc9a1186
MrXyfir <mr@xyfir.com>
2017-08-06 15:45:42 -0700

    Add commands/list-autobuy-items

---
+++ commands/list-autobuy-items.js
@@ -0,0 +1,46 @@
+const messages = require('constants/messages');
+const mysql = require('lib/mysql');
+
+/**
+ * Output autobuy items.
+ * @param {snoowrap} r
+ * @param {snoowrap.PrivateMessage} message
+ * @param {string} thread - The id of the sales thread.
+ */
+module.exports = async function(r, message, thread) {
+
+  const db = new mysql;
+
+  try {
+    await db.getConnection();
+    let rows = await db.query(
+      'SELECT author FROM sales_threads WHERE id = ?',
+      [thread]
+    );
+
+    if (!rows.length)
+      throw messages.NO_MATCHING_THREAD(thread);
+    if (rows[0].author != message.author.name)
+      throw messages.UNAUTHORIZED_COMMAND;
+
+    rows = await db.query(
+      'SELECT item FROM autobuy_items WHERE thread = ? ORDER BY added ASC',
+      [thread]
+    );
+    db.release();
+
+    if (!rows.length) throw messages.NO_AUTOBUY_ITEMS;
+
+    await message.reply(
+      rows.map(r => r.item).join('\n\n')
+    );
+  }
+  catch (err) {
+    if (typeof err != 'string')
+      return console.error('commands/list-autobuy-items', err);
+
+    db.release();
+    message.reply(err);
+  }
+
+}
