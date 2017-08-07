a71ebef0069fb323d9b336250485b98f04579d7b
MrXyfir <mr@xyfir.com>
2017-08-07 14:15:31 -0700

    Add test for some commands in commands/parse

---
+++ tests/commands/parse.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const parse = require('commands/parse');
+
+module.exports = function() {
+
+  let actual = {}, expected = {};
+
+  // REVISE
+  actual = parse({
+    was_comment: true, subject: 'username mention', body: 'revise'
+  }),
+  expected = {
+    command: 'revise'
+  };
+  assert.deepEqual(actual, expected, 'Command: revise');
+
+  // ADD AUTOBUY ITEMS
+  actual = parse({
+    body: [
+      'add autobuy items to 6s1psl',
+      'item 1',
+      'item 2'
+    ].join('\n\n')
+  }),
+  expected = {
+    command: 'add-autobuy-items',
+    thread: '6s1psl',
+    items: [
+      'item 1', 'item 2'
+    ]
+  };
+  assert.deepEqual(actual, expected, 'Command: add-autobuy-items');
+
+  // LIST AUTOBUY ITEMS
+  actual = parse({
+    body: 'list autobuy items in 6s1psl'
+  }),
+  expected = {
+    command: 'list-autobuy-items', thread: '6s1psl'
+  };
+  assert.deepEqual(actual, expected, 'Command: list-autobuy-items');
+
+  // CLEAR AUTOBUY ITEMS
+  actual = parse({
+    body: 'clear autobuy items in 6s1psl'
+  }),
+  expected = {
+    command: 'clear-autobuy-items', thread: '6s1psl'
+  };
+  assert.deepEqual(actual, expected, 'Command: clear-autobuy-items');
+
+}
