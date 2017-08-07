110e1476f8d3b01cfb0a8e77058dce62021ecfe7
MrXyfir <mr@xyfir.com>
2017-08-07 14:15:04 -0700

    Add test for lib/threads/parse

---
+++ tests/lib/threads/parse.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const parse = require('lib/threads/parse');
+
+module.exports = function() {
+
+  const data = parse({
+    title: 'Some Title',
+    selftext: [
+      '**Type** Digital Item',
+      '**Category** Digital Goods',
+      '**Description** Some description\n\ncontinued description',
+      '**Price** $5.10',
+      '**Currency** BTC PP ETH',
+      '**Images** http://i.imgur.com/XXXXXXXXXXXXXX.jpg https://i.imgur.com/XXXXXXXXXXXXXZ.png',
+      '**NSFW** True',
+      '**Verifiable** True',
+      '**Escrow** True',
+      '**Autobuy** True',
+      '**Addresses** BTC=1MyAddress ETH=0xMyAddress'
+    ].join('\n\n')
+  });
+
+  assert.deepEqual(data, {
+    title: 'Some Title',
+    type: 'Digital Item',
+    category: 'Digital Goods',
+    description: 'Some description\n\ncontinued description',
+    price: 5.1,
+    currency: ['BTC', 'PP', 'ETH'],
+    images: [
+      '[Image 1](http://i.imgur.com/XXXXXXXXXXXXXX.jpg)',
+      '[Image 2](https://i.imgur.com/XXXXXXXXXXXXXZ.png)'
+    ].join(', '),
+    nsfw: true, verifiable: true, escrow: true, autobuy: true, verified: false,
+    stock: 0,
+    addresses: {
+      BTC: '1MyAddress',
+      ETH: '0xMyAddress'
+    }
+  }, 'Parse thread with all fields');
+
+}
