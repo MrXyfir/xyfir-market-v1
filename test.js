406052fe1e81c56adb546fd5bd327239d94366e8
MrXyfir <mr@xyfir.com>
2017-08-07 17:34:56 -0700

    Add completion message

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ require('app-module-path').addPath(__dirname);
 require('tests/lib/threads/parse')();
 require('tests/lib/threads/build')();
 require('tests/commands/parse')();
+
+console.log('Tests complete');
