fcd56d94c631761a020b53b4246fb19068b13def
MrXyfir <mr@xyfir.com>
2017-08-12 16:02:17 -0700

    Add main/payment-listener

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
 require('app-module-path').addPath(__dirname);
 
+const paymentListener = require('main/payment-listener');
 const messageListener = require('main/message-listener');
 const threadListener = require('main/thread-listener');
 const updateThreads = require('main/update-threads');
+const express = require('express');
+const parser = require('body-parser');
 const config = require('constants/config');
+const app = express();
 
 messageListener();
 threadListener();
@@ -14,3 +18,13 @@ if (config.environment.type != 'dev') {
   setInterval(threadListener, 60 * 1000);  // every minute
   setInterval(updateThreads, 3600 * 1000); // every hour
 }
+
+app.use(parser.json({ limit: '5mb' }));
+app.use(parser.urlencoded({ extended: true, limit: '5mb' }));
+
+app.post('/coinbase-notifications', paymentListener);
+
+app.listen(
+  config.environment.port,
+  () => console.log('Server running on port', config.environment.port)
+);
