8164e61dabfbc92bf90ee86601569c31e4f0c70c
MrXyfir <mr@xyfir.com>
2017-08-13 14:35:45 -0700

    Add tests for new commands

--- tests/commands/parse.js
+++ tests/commands/parse.js
@@ -60,4 +60,61 @@ module.exports = function() {
   };
   assert.deepEqual(actual, expected, 'Command: remove');
 
+  // PURCHASE (on thread)
+  actual = parse({
+    was_comment: true, body: 'u/xyMarketBot purchase 1 using BTC',
+    context: '/r/xyMarket/comments/6s1psl/daily_thread/'
+  }),
+  expected = {
+    command: 'purchase',
+    escrow: false, thread: '6s1psl', quantity: 1, currency: 'BTC'
+  };
+  assert.deepEqual(actual, expected, 'Command: purchase');
+
+  // PURCHASE (in message)
+  actual = parse({
+    body: 'buy 3 of 6s1psl using LTC and escrow'
+  }),
+  expected = {
+    command: 'purchase',
+    escrow: true, thread: '6s1psl', quantity: 3, currency: 'LTC'
+  };
+  assert.deepEqual(actual, expected, 'Command: purchase');
+
+  // REQUEST ESCROW (without note)
+  actual = parse({
+    body: 'request escrow for 54321'
+  }),
+  expected = {
+    command: 'request-escrow', order: 54321, note: 'None'
+  };
+  assert.deepEqual(actual, expected, 'Command: request-escrow');
+
+  // REQUEST ESCROW (with note)
+  actual = parse({
+    body: 'request escrow for 54321\n\nsome note'
+  }),
+  expected = {
+    command: 'request-escrow', order: 54321, note: 'some note'
+  };
+  assert.deepEqual(actual, expected, 'Command: request-escrow');
+
+  // GIVE FEEDBACK
+  actual = parse({
+    body: 'give positive feedback for 54321'
+  }),
+  expected = {
+    command: 'give-feedback', order: 54321, feedback: '', type: 1
+  };
+  assert.deepEqual(actual, expected, 'Command: give-feedback');
+
+  // GIVE FEEDBACK
+  actual = parse({
+    body: 'give negative feedback for 54321 very bad!'
+  }),
+  expected = {
+    command: 'give-feedback', order: 54321, feedback: 'very bad!', type: -1
+  };
+  assert.deepEqual(actual, expected, 'Command: give-feedback');
+
 }
