773a216398de91197553f61dfbaca1e190834554
MrXyfir <mr@xyfir.com>
2017-11-11 15:10:29 -0800

    Modify `main/payment-listener` to use new payment system

--- main/payment-listener.js
+++ main/payment-listener.js
@@ -1,210 +1,189 @@
+const validateTransaction = require('lib/blockchain/validate-transaction');
 const updateThread = require('lib/threads/update');
 const orderStatus = require('constants/types/order-statuses');
 const orderTypes = require('constants/types/orders');
-const sendMoney = require('lib/coinbase/send-money');
 const templates = require('constants/templates');
 const config = require('constants/config');
-const moment = require('moment');
-const mysql = require('lib/mysql');
+const MySQL = require('lib/mysql');
 const snoo = require('snoowrap');
 
 const r = new snoo(config.snoowrap);
 
-/*
-  POST coinbase-notifications
-  REQUIRED
-    https://developers.coinbase.com/api/v2?javascript#notifications
-    {
-      type: string,
-      data: {
-        name: string, created_at: date-string
-      },
-      additional_data: {
-        amount: string, currency: string
-      }
-    }
-  RETURN
-    HTTP status 200 or 400
-  DESCRIPTION
-    Handles new payments received by any of the Coinbase addresses
-*/
-module.exports = async function(req, res) {
+/**
+ * Handles payments that are awaiting confirmation.
+ */
+module.exports = async function() {
 
-  const { body } = req;
-  const db = new mysql;
+  const db = new MySQL;
 
   try {
-    if (body.type != 'wallet:addresses:new-payment')
-      throw 'ignore - unwanted type';
-
-    const { name, created_at: created } = body.data;
-    const { amount, currency } = body.additional_data.amount;
-
-    // Payment isn't for xyMarket
-    if (!name || name.indexOf('r/xyMarket') != 0)
-      throw 'ignore - non-xyMarket address';
-
     await db.getConnection();
-    let rows = await db.query(`
+    const orders = await db.query(`
       SELECT
-        id, type, thread, buyer, escrow, quantity, created, amountForSeller
+        id, type, thread, buyer, escrow, quantity, currency,
+        amount, transaction
       FROM orders
       WHERE
-        id = ? AND currency = ? AND amountForBuyer <= ? AND status = ?
+        status = ? AND created > DATE_SUB(NOW(), INTERVAL 1 HOUR)
     `, [
-      name.split(' ')[1], currency, amount, orderStatus.UNPAID
+      orderStatus.AWAITING_CONFIRMATIONS
     ]);
 
-    if (!rows.length) throw 'ignore - no matching order';
+    if (!orders.length) return;
 
-    const order = rows[0];
-
-    if (
-      // Convert order creation date from current timezone, to UTC
-      // Add 16 minutes to get expiration date and then convert to unix time
-      moment(order.created).utc().add(16, 'minutes').unix() <
-      // Convert Coinbase's UTC timestamp to unix
-      moment.utc(created).unix()
-    ) throw 'ignore - order expired';
-
-    // Get needed data from thread
-    rows = await db.query(
-      'SELECT id, author, data FROM sales_threads WHERE id = ?',
-      [order.thread]
-    );
-
-    const thread = rows[0];
-    thread.data = JSON.parse(thread.data);
-
-    // Handle thread promotion
-    if (order.type == orderTypes.PROMOTE_THREAD) {
-      // Mark order as complete
-      await db.query(
-        'UPDATE orders SET status = ?, completed = NOW() WHERE id = ?',
-        [orderStatus.COMPLETE, order.id]
+    for (let order of orders) {
+      // Get needed data from thread
+      const [thread] = await db.query(
+        'SELECT id, author, data FROM sales_threads WHERE id = ?',
+        [order.thread]
       );
+      thread.data = JSON.parse(thread.data);
 
-      // Mark thread as promoted
-      await db.query(`
-        UPDATE sales_threads
-        SET promoted = DATE_ADD(NOW(), INTERVAL ? MONTH)
-        WHERE id = ?
-      `, [
-        order.quantity, thread.id
-      ]);
+      /* Handle thread promotion */
+      if (order.type == orderTypes.PROMOTE_THREAD) {
+        try {
+          // Validate confirmations of transaction
+          await validateTransaction({
+            transaction: order.transaction,
+            confirmed: true,
+            receiver: config.ids.addresses[order.currency],
+            currency: order.currency,
+            amount: order.amount
+          });
+        }
+        catch (err) { continue; }
 
-      // Notify user their thread has been promoted
-      await r.composeMessage({
-        subject: 'Thread Promoted',
-        text: templates.THREAD_PROMOTED(thread.id, order.quantity),
-        to: order.buyer
-      });
-    }
-    // Handle normal purchase
-    else if (order.type == orderTypes.PURCHASE) {
-      if (thread.data.autobuy) {
-        // Load enough autobuy items to satisfy order
-        rows = await db.query(`
-          SELECT id, item FROM autobuy_items WHERE thread = ?
-          ORDER BY added ASC LIMIT ?
-        `, [
-          thread.id,
-          order.quantity
-        ]);
-  
-        // Delete autobuy items from database
-        if (rows.length) {
-          await db.query(
-            'DELETE FROM autobuy_items WHERE id IN (?)',
-            [rows.map(r => r.id)]
-          );
-        }
-  
-        thread.autobuyItems = rows.map(r => r.item);
-      }
-  
-      const messagesToSeller = [
-        templates.SELLER_RECEIVES_ORDER({
-          quantity: order.quantity,
-          currency,
-          amount: order.amountForSeller,
-          thread: thread.id,
-          order: order.id,
-          buyer: order.buyer
-        })
-      ],
-      messagesToBuyer = [
-        templates.BUYER_SENDS_PAYMENT(order.id)
-      ];
-  
-      if (thread.data.autobuy) {
-        // Add autobuy items to buyer's message
-        if (thread.autobuyItems.length) {
-          messagesToBuyer.push(
-            templates.GIVE_AUTOBUY_ITEMS(thread.autobuyItems)
-          );
-        }
-  
-        // Not enough autobuy items to complete order
-        if (thread.autobuyItems.length < order.quantity) {
-          const itemsMissing = order.quantity - thread.autobuyItems.length;
-  
-          messagesToSeller.push(templates.AUTOBUY_ITEMS_REQUIRED(itemsMissing));
-          messagesToBuyer.push(templates.AUTOBUY_ITEMS_OWED(itemsMissing));
-        }
-  
-        thread.data.stock -= thread.autobuyItems.length;
-        await updateThread(r, thread);
-      }
-  
-      if (order.escrow) {
-        messagesToSeller.push(templates.SELLER_ORDER_IN_ESCROW(order.id));
-        messagesToBuyer.push(templates.BUYER_ORDER_IN_ESCROW(order.id));
-  
-        await db.query(
-          'UPDATE orders SET status = ? WHERE id = ?',
-          [orderStatus.IN_ESCROW, order.id]
-        );
-      }
-      else {
-        messagesToSeller.push(templates.ORDER_COMPLETE(order.id));
-        messagesToBuyer.push(templates.ORDER_COMPLETE(order.id));
-  
-        // Pay seller
-        await sendMoney(
-          order.amountForSeller, currency,
-          thread.data.addresses[currency]
-        );
-  
+        // Mark order as complete
         await db.query(
           'UPDATE orders SET status = ?, completed = NOW() WHERE id = ?',
           [orderStatus.COMPLETE, order.id]
         );
+
+        // Mark thread as promoted
+        await db.query(`
+          UPDATE sales_threads
+          SET promoted = DATE_ADD(NOW(), INTERVAL ? MONTH)
+          WHERE id = ?
+        `, [
+          order.quantity, thread.id
+        ]);
+
+        // Notify user their thread has been promoted
+        await r.composeMessage({
+          subject: 'Thread Promoted',
+          text: templates.THREAD_PROMOTED(thread.id, order.quantity),
+          to: order.buyer
+        });
       }
-  
-      await r.composeMessage({
-        to: order.buyer,
-        subject: 'Order ' + order.id,
-        text: messagesToBuyer.join('\n\n')
-      });
-  
-      await r.composeMessage({
-        to: thread.author,
-        subject: 'Order ' + order.id,
-        text: messagesToSeller.join('\n\n')
-      });
+      /* Handle normal purchase */
+      else if (order.type == orderTypes.PURCHASE) {
+        try {
+          // Validate confirmations of transaction
+          await validateTransaction({
+            transaction: order.transaction,
+            confirmed: true,
+            receiver: thread.data.addresses[order.currency],
+            currency: order.currency,
+            amount: order.amount
+          });
+        }
+        catch (err) { continue; }
+
+        if (thread.data.autobuy) {
+          // Load enough autobuy items to satisfy order
+          rows = await db.query(`
+            SELECT id, item FROM autobuy_items WHERE thread = ?
+            ORDER BY added ASC LIMIT ?
+          `, [
+            thread.id,
+            order.quantity
+          ]);
+    
+          // Delete autobuy items from database
+          if (rows.length) {
+            await db.query(
+              'DELETE FROM autobuy_items WHERE id IN (?)',
+              [rows.map(r => r.id)]
+            );
+          }
+    
+          thread.autobuyItems = rows.map(r => r.item);
+        }
+    
+        const messagesToSeller = [
+          templates.SELLER_RECEIVES_ORDER({
+            quantity: order.quantity,
+            currency: order.currency,
+            amount: order.amount,
+            thread: thread.id,
+            order: order.id,
+            buyer: order.buyer
+          })
+        ],
+        messagesToBuyer = [
+          templates.BUYER_SENDS_PAYMENT(order.id)
+        ];
+    
+        if (thread.data.autobuy) {
+          // Add autobuy items to buyer's message
+          if (thread.autobuyItems.length) {
+            messagesToBuyer.push(
+              templates.GIVE_AUTOBUY_ITEMS(thread.autobuyItems)
+            );
+          }
+    
+          // Not enough autobuy items to complete order
+          if (thread.autobuyItems.length < order.quantity) {
+            const itemsMissing = order.quantity - thread.autobuyItems.length;
+    
+            messagesToSeller.push(
+              templates.AUTOBUY_ITEMS_REQUIRED(itemsMissing)
+            );
+            messagesToBuyer.push(templates.AUTOBUY_ITEMS_OWED(itemsMissing));
+          }
+    
+          thread.data.stock -= thread.autobuyItems.length;
+          await updateThread(r, thread);
+        }
+    
+        if (order.escrow) {
+          messagesToSeller.push(templates.SELLER_ORDER_IN_ESCROW(order.id));
+          messagesToBuyer.push(templates.BUYER_ORDER_IN_ESCROW(order.id));
+    
+          await db.query(
+            'UPDATE orders SET status = ? WHERE id = ?',
+            [orderStatus.IN_ESCROW, order.id]
+          );
+        }
+        else {
+          messagesToSeller.push(templates.ORDER_COMPLETE(order.id));
+          messagesToBuyer.push(templates.ORDER_COMPLETE(order.id));
+    
+          await db.query(
+            'UPDATE orders SET status = ?, completed = NOW() WHERE id = ?',
+            [orderStatus.COMPLETE, order.id]
+          );
+        }
+    
+        await r.composeMessage({
+          to: order.buyer,
+          subject: 'Order ' + order.id,
+          text: messagesToBuyer.join('\n\n')
+        });
+    
+        await r.composeMessage({
+          to: thread.author,
+          subject: 'Order ' + order.id,
+          text: messagesToSeller.join('\n\n')
+        });
+      }
     }
 
     db.release();
-    res.status(200).send();
   }
   catch (err) {
     db.release();
-
-    if (/^ignore/.test(err))
-      res.status(200).send();
-    else 
-      res.status(400).send();
+    console.error('main/payment-listener', err);
   }
 
 }
--- start.js
+++ start.js
@@ -5,16 +5,15 @@ const messageListener = require('main/message-listener');
 const threadListener = require('main/thread-listener');
 const updateThreads = require('main/update-threads');
 const postFinder = require('main/post-finder');
-const express = require('express');
-const parser = require('body-parser');
 const config = require('constants/config');
-const app = express();
 
+paymentListener();
 messageListener();
 threadListener();
 updateThreads();
 
 if (config.environment.type != 'dev') {
+  setInterval(paymentListener, 60 * 1000); // every minute
   setInterval(messageListener, 60 * 1000); // every minute
   setInterval(threadListener, 60 * 1000);  // every minute
   setInterval(updateThreads, 3600 * 1000); // every hour
@@ -22,13 +21,3 @@ if (config.environment.type != 'dev') {
 
   postFinder();
 }
-
-app.use(parser.json({ limit: '5mb' }));
-app.use(parser.urlencoded({ extended: true, limit: '5mb' }));
-
-app.post('/coinbase-notifications', paymentListener);
-
-app.listen(
-  config.environment.port,
-  () => console.log('Server running on port', config.environment.port)
-);
