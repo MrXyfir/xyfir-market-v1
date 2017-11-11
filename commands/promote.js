f83dbcfc8face6ca89df975c6c8ccbe379b19df2
MrXyfir <mr@xyfir.com>
2017-11-11 15:16:29 -0800

    Use new payment system

--- commands/promote.js
+++ commands/promote.js
@@ -1,8 +1,9 @@
 const convertCurrency = require('lib/coinbase/convert-currency');
-const generateAddress = require('lib/coinbase/generate-address');
+const orderStatus = require('constants/types/order-statuses');
 const orderTypes = require('constants/types/orders');
 const templates = require('constants/templates');
-const mysql = require('lib/mysql');
+const config = require('constants/config');
+const MySQL = require('lib/mysql');
 
 /**
  * @typedef {object} PromoteCommand
@@ -10,21 +11,20 @@ const mysql = require('lib/mysql');
  * @prop {number} months
  * @prop {string} currency
  */
-
 /**
  * Begin process of promoting a sales thread.
- * @param {snoowrap} r
- * @param {snoowrap.PrivateMessage|snoowrap.Comment} message
+ * @param {Snoowrap} r
+ * @param {Snoowrap.PrivateMessage|Snoowrap.Comment} message
  * @param {PromoteCommand} command
  */
 module.exports = async function(r, message, command) {
 
-  const db = new mysql;
+  const db = new MySQL;
 
   try {
     // Make sure thread exists, is approved, not removed, user is creator
     await db.getConnection();
-    const rows = await db.query(`
+    let rows = await db.query(`
       SELECT id FROM sales_threads
       WHERE id = ? AND author = ? AND approved = ? AND removed = ?
     `, [
@@ -47,17 +47,38 @@ module.exports = async function(r, message, command) {
 
     if (!orderId) throw templates.UNEXPECTED_ERROR;
 
-    // Generate appropriate temporary address
-    const {address} = await generateAddress(command.currency, orderId);
+    // Get system's payment address for currency
+    const address = config.ids.addresses[command.currency];
     
-    // Calculate amount
-    const amount = await convertCurrency(
-      2 * command.months, 'USD', command.currency
+    // Calculate amount: $2.50 per month
+    let amount = await convertCurrency(
+      2.5 * command.months, 'USD', command.currency
     );
+
+    // Get amounts for recent unpaid 'promote thread' orders for same currency
+    rows = await db.query(`
+      SELECT amount
+      FROM orders
+      WHERE
+        created > DATE_SUB(NOW(), INTERVAL 1 HOUR) AND
+        status = ? AND type = ? AND currency = ?
+    `, [
+      orderStatus.UNPAID, orderTypes.PROMOTE_THREAD, command.currency
+    ]);
+
+    // xyMarket is currently waiting for payments of these amounts to the same
+    // address that the buyer will be sending to
+    const avoidAmounts = rows.map(r => +r.amount);
+
+    // Increase amount until unique
+    while (avoidAmounts.indexOf(amount) > -1) {
+      amount += 0.00000001;
+      amount = +amount.toFixed(8);
+    }
 
     // Update amount in orders table
     await db.query(
-      'UPDATE orders SET amountForBuyer = ? WHERE id = ?',
+      'UPDATE orders SET amount = ? WHERE id = ?',
       [amount, orderId]
     );
     db.release();
@@ -66,6 +87,7 @@ module.exports = async function(r, message, command) {
     await message.reply(
       templates.SEND_PAYMENT({
         currency: command.currency,
+        orderId,
         address,
         amount
       })
@@ -74,8 +96,7 @@ module.exports = async function(r, message, command) {
   catch (err) {
     db.release();
 
-    if (typeof err != 'string')
-      return console.error('commands/promote', err);
+    if (typeof err != 'string') return console.error('commands/promote', err);
 
     message.reply(err);
   }
