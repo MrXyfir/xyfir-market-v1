2069d865d77de8b8d72f54c94f362bb5a5cf40d7
MrXyfir <mr@xyfir.com>
2017-11-16 13:55:18 -0800

    Add `main/stats-manager`

---
+++ main/stats-manager.js
@@ -0,0 +1,57 @@
+const createUserStatsThread = require('lib/users/stats-thread/create');
+const updateUserStatsThread = require('lib/users/stats-thread/update');
+const templates = require('constants/templates');
+const config = require('constants/config');
+const MySQL = require('lib/mysql');
+const snoo = require('snoowrap');
+
+const r = new snoo(config.snoowrap);
+
+module.exports = async function() {
+
+  const db = new MySQL;
+
+  try {
+    await db.getConnection();
+    const users = await db.query(`
+      SELECT name, joined, statsThread
+      FROM users
+      WHERE NOW() > statsThreadExpires
+    `);
+
+    for (let user of users) {
+      // Create new stats thread
+      const newThreadId = await createUserStatsThread(user.name);
+
+      // Update statsThread, statsThreadExpires in users table
+      await db.query(`
+        UPDATE users SET
+          statsThread = ?,
+          statsThreadExpires = DATE_ADD(NOW(), INTERVAL 5 MONTH)
+        WHERE
+          name = ?
+      `, [
+        newThreadId,
+        user.name
+      ]);
+
+      const oldThreadId = user.statsThread;
+      user.statsThread = newThreadId;
+
+      // Update new thread
+      await updateUserStatsThread(user, db);
+
+      // Update old stats thread to point to new one
+      await r
+        .getSubmission(oldThreadId)
+        .edit(templates.USER_STATS_THREAD_MOVED(newThreadId));
+    }
+
+    db.release();
+  }
+  catch (err) {
+    db.release();
+    console.error('main/stats-manager', err);
+  }
+
+}
--- start.js
+++ start.js
@@ -4,6 +4,7 @@ const paymentListener = require('main/payment-listener');
 const messageListener = require('main/message-listener');
 const threadListener = require('main/thread-listener');
 const updateThreads = require('main/update-threads');
+const statsManager = require('main/stats-manager');
 const postFinder = require('main/post-finder');
 const config = require('constants/config');
 
@@ -11,12 +12,14 @@ paymentListener();
 messageListener();
 threadListener();
 updateThreads();
+statsManager();
 
 if (config.environment.type != 'dev') {
   setInterval(paymentListener, 60 * 1000); // every minute
   setInterval(messageListener, 60 * 1000); // every minute
   setInterval(threadListener, 60 * 1000);  // every minute
   setInterval(updateThreads, 3600 * 1000); // every hour
+  setInterval(statsManager, 86400 * 1000); // every day
   setInterval(postFinder, 3600 * 1000);    // every hour
 
   postFinder();
